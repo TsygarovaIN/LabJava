4c305ef79a09fb1b944926e07679dfb23f75e324
TsygarovaIN <55764818+TsygarovaIN@users.noreply.github.com>
2020-04-29 02:43:38 +0300

    lab3

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -7,15 +7,15 @@ public class Main {
     private static ConcurrentLinkedQueue<StudentClass> studentQueue = new ConcurrentLinkedQueue<StudentClass>();
     private static StudentGenerator studentGenerator = new StudentGenerator();
 
-    private static Semaphore sem = new Semaphore(1, true);
+    private static Semaphore semaphore = new Semaphore(1, true);
 
-    private static RobotClass robot1 = new RobotClass("Mathematics", studentQueue, sem);
-    private static RobotClass robot2 = new RobotClass("OOP", studentQueue, sem);
-    private static RobotClass robot3 = new RobotClass("Physics", studentQueue, sem );
+    private static RobotClass robot1 = new RobotClass("Mathematics", studentQueue, semaphore);
+    private static RobotClass robot2 = new RobotClass("OOP", studentQueue, semaphore);
+    private static RobotClass robot3 = new RobotClass("Physics", studentQueue, semaphore);
 
     public static void main(String[] args){
         studentGenerator.start();
-        studentGenerator.setResource(studentQueue);
+        studentGenerator.setQueue(studentQueue);
         robot1.start();
         robot2.start();
         robot3.start();
