03c48aae970c3b32543818f6be788ccd5c6ea628
TsygarovaIN <55764818+TsygarovaIN@users.noreply.github.com>
2020-11-07 21:45:51 +0300

    Delete ClientsRepository.java

--- Library/src/main/java/main/repos/ClientsRepository.java
+++
@@ -1,7 +0,0 @@
-package main.repos;
-
-import main.entity.Clients;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientsRepository extends CrudRepository<Clients,Long> {
-}
