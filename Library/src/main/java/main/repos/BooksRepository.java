5256b5da672110671c27ea0c2a3ea9e824ee4d94
TsygarovaIN <55764818+TsygarovaIN@users.noreply.github.com>
2020-11-07 21:45:33 +0300

    Delete BooksRepository.java

--- Library/src/main/java/main/repos/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package main.repos;
-
-import main.entity.Books;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BooksRepository extends CrudRepository<Books,Long> {
-}
