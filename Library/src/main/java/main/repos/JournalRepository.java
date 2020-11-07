09bde5eb5b4f411e4c50086b2b5716f65b425f18
TsygarovaIN <55764818+TsygarovaIN@users.noreply.github.com>
2020-11-07 21:45:38 +0300

    Delete JournalRepository.java

--- Library/src/main/java/main/repos/JournalRepository.java
+++
@@ -1,7 +0,0 @@
-package main.repos;
-
-import main.entity.Journal;
-import org.springframework.data.repository.CrudRepository;
-
-public interface JournalRepository extends CrudRepository<Journal,Long> {
-}
