9772c3df1f9ea4ee6f44638fed610514205a1f6e
TsygarovaIN <55764818+TsygarovaIN@users.noreply.github.com>
2020-11-07 21:45:20 +0300

    Delete BookTypesRepository.java

--- Library/src/main/java/main/repos/BookTypesRepository.java
+++
@@ -1,8 +0,0 @@
-package main.repos;
-
-
-import main.entity.BookTypes;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookTypesRepository extends CrudRepository<BookTypes,Long> {
-}
