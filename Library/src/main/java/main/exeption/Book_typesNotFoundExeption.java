fb6b578ec8de314a353484b0861eb7369b096565
TsygarovaIN <55764818+TsygarovaIN@users.noreply.github.com>
2020-11-07 21:48:48 +0300

    Delete Book_typesNotFoundExeption.java

--- Library/src/main/java/main/exeption/Book_typesNotFoundExeption.java
+++
@@ -1,7 +0,0 @@
-package main.exeption;
-
-public class Book_typesNotFoundExeption extends RuntimeException{
-    public Book_typesNotFoundExeption(String message) {
-        super(message);
-    }
-}
